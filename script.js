adf76c7eeda63656f90cf7e9ddcca46451dab4b4
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-04-20 03:47:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function removeColor() {
+    const selectElement = document.getElementById("colorSelect");
+    const selectedIndex = selectElement.selectedIndex;
+    if (selectedIndex !== -1) {
+      selectElement.remove(selectedIndex);
+    }
+  }
+  
+  const removeButton = document.querySelector('input[type="button"][value="Select and Remove"]');
+  removeButton.addEventListener("click", removeColor);
+  
